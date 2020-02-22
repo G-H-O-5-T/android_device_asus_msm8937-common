33c0d13a51539ee4dca8ac2a131dc4c4e3210a7d
dianlujitao <dianlujitao@lineageos.org>
2020-02-22 11:30:37 +0800

    msm8937-common: Copy extra system libs to vendor
    Change-Id: I0a086920c855da3a39e8a1c902a206d60a743495

--- msm8937.mk
+++ msm8937.mk
@@ -356,6 +356,9 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
 
+PRODUCT_PACKAGES += \
+    vndk-ext
+
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
