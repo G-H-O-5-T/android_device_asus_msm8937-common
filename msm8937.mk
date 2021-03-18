c8a64b73898fd83146e54db8a80980a6c1da706c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-19 00:30:49 +0100

    msm8937-common: Build some libs from source
    Change-Id: I4a5a7668162575e7424212bc970820dcb26131e3

--- msm8937.mk
+++ msm8937.mk
@@ -81,13 +81,15 @@ PRODUCT_PACKAGES += \
 	audio.primary.msm8937 \
     audio.r_submix.default \
     audio.usb.default \
+    libaudiopreprocessing \
     libaudioroute \
     libaacwrapper \
     libaudio-resampler \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
-    libvolumelistener
+    libvolumelistener \
+    libtinycompress
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
@@ -110,6 +112,10 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
 
 # Camera
 PRODUCT_PACKAGES += \
